c06e44cde2c3186ee9915506654dfcdf8298670d
Gakmannn <gnn87@mail.ru>
2024-09-12 20:28:37 +0300

    root

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from '#root/App.vue'
 import HeaderComp from './components/HeaderComponent.vue'
 import {router} from './routes'
 
